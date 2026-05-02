1d933b05a372298c3bcd9ccc8375d1109f316b92
uramos89 <uramos@sistemascontino.com.mx>
2026-05-02 07:35:17 -0600

    S-02 & D-02: Removing hardcoded credentials and dead code from repository.

--- scratch/check-db.js
+++
@@ -1,6 +0,0 @@
-import Database from "better-sqlite3";
-const db = new Database("saturn.db");
-const server = db.prepare("SELECT * FROM servers WHERE id = 'srv-192-168-174-130'").get();
-const ssh = db.prepare("SELECT * FROM ssh_connections WHERE serverId = 'srv-192-168-174-130'").get();
-console.log("Server:", JSON.stringify(server, null, 2));
-console.log("SSH:", JSON.stringify(ssh, null, 2));
